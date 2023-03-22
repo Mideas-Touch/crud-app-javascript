97592e9e206499408a636ae570abaed1f42cf11b
flipblip <wickynjy@gmail.com>
2023-03-22 19:10:01 +0300

    get what we'll work with in js

---
+++ index.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('btn');
+
+const posts = document.getElementById('posts-list')
+
+const edit = document.getElementsByClassName('fa-edit')
+
+const deletePost = document.getElementsByClassName('fa-trash-alt')
+
+
+console.log(button, posts, edit, deletePost)
