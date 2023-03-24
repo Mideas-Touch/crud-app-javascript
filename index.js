002982dd65ac3ba30716098b386faf08cb5af9d3
flipblip <wickynjy@gmail.com>
2023-03-24 18:43:50 +0300

    Can now display a list of posts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let deletePost = document.getElementsByClassName('fa-trash-alt')
 
 
 // Create an array with post objects
-let data = [{id: 1, content: ''}]
+let data = [{id: 1, content: 'First post'}, {id: 2, content: 'Second post'}]
 
  
 
@@ -31,6 +31,29 @@ function create(newPost){
     data.push(newPost)
     console.log(data)
 }
+
+// Display mapped posts on the document
+
+const displayPosts = () =>{
+    // console.log(data)
+    data.map(post => {
+        // console.log(post)
+        const display =
+        `<div id="post">
+            <p>${post.content}</p>
+            <span class="options">
+                <i class="fas fa-edit"></i>
+                <i class="fas fa-trash-alt"></i>
+            </span>
+        </div>`
+
+        posts.innerHTML += display
+    })
+
+
+}
+   
+
 
 
 
