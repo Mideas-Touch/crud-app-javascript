b083e05a8c299e5a88f59e15268b101e04c3ce23
flipblip <wickynjy@gmail.com>
2023-03-24 12:24:17 +0300

    Work on create functionality

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const button = document.getElementById('btn');
+let button = document.getElementById('btn');
+let posts = document.getElementById('posts-list')
+let edit = document.getElementsByClassName('fa-edit')
+let deletePost = document.getElementsByClassName('fa-trash-alt')
+const text = document.getElementById('msg')
+let form = document.getElementById('form')
 
-const posts = document.getElementById('posts-list')
+// Create an array with post objects
+let data = [{id: 1, content: ''}]
 
-const edit = document.getElementsByClassName('fa-edit')
+ 
 
-const deletePost = document.getElementsByClassName('fa-trash-alt')
+// Add eventListeneer to the buttn
+button.addEventListener('click', function(e){
+     create()
+})
+
+// A function to handle submitting post
+
+let newPost;
+
+// Increment id
+
+function increment(){
+    let newId = data.length +1
+    return newId
+}
+
+function create(e, newPost, newId){
+    e.preventDefault()
+    increment()
+    newPost = {id: newId, content: 'This is my third post'}
+    data.push(newPost)
+    console.log(data)
+    console.log(data.length)
+    console.log(text.value)
+}
+
+// create()
 
 
-console.log(button, posts, edit, deletePost)
+
+
+
+
